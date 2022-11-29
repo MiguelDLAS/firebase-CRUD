c09d3258c70cf110bfe332cb03f79457c998a739
MiguelDLAS <mikiserrano2018@gmail.com>
2022-11-28 20:20:26 -0600

    conexion

---
+++ index.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  import { } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCQaOWgSz49Nz5AA1hmL2WTzKaPy6mA_uQ",
+    authDomain: "fir-javascript-crud-f2162.firebaseapp.com",
+    projectId: "fir-javascript-crud-f2162",
+    storageBucket: "fir-javascript-crud-f2162.appspot.com",
+    messagingSenderId: "413864041879",
+    appId: "1:413864041879:web:c83da1ce380ec91b1ff8d2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
