a1bceb5512bf4d2737ab8327b21c01192be53fd1
MiguelDLAS <mikiserrano2018@gmail.com>
2022-11-28 23:07:27 -0600

    agregar

--- firebase.js
+++ firebase.js
@@ -1,6 +1,11 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
- import { } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+ import { 
+     getFirestore,
+     collection, 
+     addDoc,
+     getDocs,
+ } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,4 +20,30 @@
  };
 
  // Initialize Firebase
- const app = initializeApp(firebaseConfig);
+ export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
+
+/**
+ * Save a New Task in Firestore
+ * @param {string} title the title of the Task
+ * @param {string} description the description of the Task
+ */
+export const saveTask = (title, description) =>
+  addDoc(collection(db, "tasks"), { title, description });
+
+export const onGetTasks = (callback) =>
+  onSnapshot(collection(db, "tasks"), callback);
+
+/**
+ *
+ * @param {string} id Task ID
+ */
+export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
+
+export const getTask = (id) => getDoc(doc(db, "tasks", id));
+
+export const updateTask = (id, newFields) =>
+  updateDoc(doc(db, "tasks", id), newFields);
+
+export const getTasks = () => getDocs(collection(db, "tasks"));
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+import {saveTask,getTasks} from './firebase.js'
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  import { } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+window.addEventListener('DOMContentLoaded', ()=>{
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCQaOWgSz49Nz5AA1hmL2WTzKaPy6mA_uQ",
-    authDomain: "fir-javascript-crud-f2162.firebaseapp.com",
-    projectId: "fir-javascript-crud-f2162",
-    storageBucket: "fir-javascript-crud-f2162.appspot.com",
-    messagingSenderId: "413864041879",
-    appId: "1:413864041879:web:c83da1ce380ec91b1ff8d2"
-  };
+   const querySnapshot = getTasks()
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+})
+
+const taskForm = document.getElementById('task-form')
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+   const title = taskForm['task-title']
+   const description = taskForm['task-description']
+
+   saveTask(title.value, description.value)
+
+   taskForm.reset()
+
+})
